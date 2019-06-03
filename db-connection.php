3b1f10014ac94d3fc4009068dd593ba4fd577c27
yinliuxuan <yinliuxuan@gmail.com>
2019-06-03 21:30:05 +0800

    Database Connection

---
+++ db-connection.php
@@ -0,0 +1,78 @@
+<?php
+
+class DB
+{
+    private $conn;
+
+    public function __construct($serverName, $userName, $password, $dbName)
+    {
+        $this->conn = new mysqli($serverName, $userName, $password, $dbName);
+        $this->conn->query('set names utf8');
+        if ($this->conn->connect_error) {
+            die("连接失败: " . $this->conn->connect_error."\n");
+        }
+        echo "连接成功\n";
+    }
+
+    function __destruct()
+    {
+        $this->conn->close();
+    }
+
+    public function insertBlog($title, $short_desc, $content)
+    {
+        $sql = $this->conn->prepare("INSERT INTO Blog (title,short_desc,content) VALUE (?,?,?)");
+        $sql->bind_param("sss", $title, $short_desc, $content);
+        if ($sql->execute() === TRUE) {
+            echo "新记录插入成功\n";
+        } else {
+            echo "Error: " . $sql . "<br>" . $this->conn->error;
+        }
+    }
+//
+//    public function getBlogNum(){
+//        $sql="COUNT(idBlog) FROM Blog";
+//        $result=$this->conn->query($sql);
+//        echo $result==NULL;
+////        $count=$result->fetch_assoc();
+////        return $count;
+//    }
+
+    public function getBlogList()
+    {
+        $sql="SELECT idBlog, title, short_desc FROM Blog";
+        $result=$this->conn->query($sql);
+
+        $list=array();
+        while($row=$result->fetch_assoc()){
+            $tempArray=array($row['idBlog'],$row['title'],$row['short_desc']);
+            array_push($list,$tempArray);
+        }
+        return $list;
+        //TODO 异常处理
+//        if ($result != NULL) {
+//            echo "新记录插入成功";
+//        } else {
+//            echo "Error: " . $sql . "<br>" . $this->conn->error;
+//        }
+    }
+
+    public function getBlogContent($idBlog){
+        $sql=$this->conn->prepare("SELECT * from Blog WHERE idBLog=?");
+        $sql->bind_param("i",$idBlog);
+        $sql->execute();
+        $sql->store_result();
+        $sql->bind_result($id,$title,$short_desc,$content);
+        $sql->fetch();
+        $result=array($id,$title,$short_desc,$content);
+
+        return $result;
+    }
+}
+
+$serverName = "localhost";
+$userName = "root";
+$password = "root";
+$dbName = "web";
+$db = new DB($serverName, $userName, $password, $dbName);
+
